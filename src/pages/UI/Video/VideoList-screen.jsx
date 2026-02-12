ab599dfe21614c87cba8b588fdbc1c7348eeee70
DjakaIT <danielrajic26@outlook.com>
2026-02-12 21:12:46 +0100

    backend added to structure, fetching data from youtube and outputting in a specified screen

--- src/pages/UI/Video/VideoList-screen.jsx
+++ src/pages/UI/Video/VideoList-screen.jsx
@@ -1,28 +1,30 @@
 import VideoCard from "../../../components/VideoCard";
+import { View, FlatList, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { getVideosByCategory } from "../../../../backend/data/videos";
 
-export default function VideoScreen() {
+export default function VideoScreen({ route, navigation }) {
+  const { category } = route.params;
+  const videos = getVideosByCategory(category);
+
   return (
     <>
-      <VideoCard
-        title="Full Body Workout at Home"
-        image="https://imgs.search.brave.com/wHvVQwiojM8vlNGeI7_2MYGg77HYguw21eys1Q8gOlw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/bXVzY2xlYW5kc3Ry/ZW5ndGguY29tL3Np/dGVzL2RlZmF1bHQv/ZmlsZXMvZmllbGQv/ZmVhdHVyZS1pbWFn/ZS93b3Jrb3V0LzQt/ZGF5LWF0LWhvbWUt/d29ya291dC1mb3It/d29tZW4uanBn"
-        onPress={() => console.log("Play")}
-      />
-      <VideoCard
-        title="Full Body Workout at Home"
-        image="https://imgs.search.brave.com/wHvVQwiojM8vlNGeI7_2MYGg77HYguw21eys1Q8gOlw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/bXVzY2xlYW5kc3Ry/ZW5ndGguY29tL3Np/dGVzL2RlZmF1bHQv/ZmlsZXMvZmllbGQv/ZmVhdHVyZS1pbWFn/ZS93b3Jrb3V0LzQt/ZGF5LWF0LWhvbWUt/d29ya291dC1mb3It/d29tZW4uanBn"
-        onPress={() => console.log("Play")}
-      />
-      <VideoCard
-        title="Full Body Workout at Home"
-        image="https://imgs.search.brave.com/wHvVQwiojM8vlNGeI7_2MYGg77HYguw21eys1Q8gOlw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/bXVzY2xlYW5kc3Ry/ZW5ndGguY29tL3Np/dGVzL2RlZmF1bHQv/ZmlsZXMvZmllbGQv/ZmVhdHVyZS1pbWFn/ZS93b3Jrb3V0LzQt/ZGF5LWF0LWhvbWUt/d29ya291dC1mb3It/d29tZW4uanBn"
-        onPress={() => console.log("Play")}
-      />
-      <VideoCard
-        title="Full Body Workout at Home"
-        image="https://imgs.search.brave.com/wHvVQwiojM8vlNGeI7_2MYGg77HYguw21eys1Q8gOlw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/bXVzY2xlYW5kc3Ry/ZW5ndGguY29tL3Np/dGVzL2RlZmF1bHQv/ZmlsZXMvZmllbGQv/ZmVhdHVyZS1pbWFn/ZS93b3Jrb3V0LzQt/ZGF5LWF0LWhvbWUt/d29ya291dC1mb3It/d29tZW4uanBn"
-        onPress={() => console.log("Play")}
-      />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={videos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <VideoCard
+              video={item}
+              onPress={() => navigation.navigate("Video", { video: item })}
+            />
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#FAFAFA" },
+});
