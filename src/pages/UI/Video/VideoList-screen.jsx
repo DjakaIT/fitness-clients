6d3d749d083e0add4873f5dc9d66b863d6859e09
DjakaIT <danielrajic26@outlook.com>
2026-02-14 18:50:49 +0100

    passed image property as dynamic url which extracts thumbnails for corresponding video, and the title, the card now complete

--- src/pages/UI/Video/VideoList-screen.jsx
+++ src/pages/UI/Video/VideoList-screen.jsx
@@ -13,6 +13,9 @@ export default function VideoScreen({ route, navigation }) {
         <FlatList
           data={videos}
           keyExtractor={(item) => item.id}
+          containerCardsStyle={styles.containerCards}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
           renderItem={({ item }) => (
             <VideoCard
               //TODO: fill the whole videos.js object file with names, ids, categories.
@@ -29,5 +32,14 @@ export default function VideoScreen({ route, navigation }) {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#FAFAFA" },
+  container: {
+    flex: 1,
+    backgroundColor: "#FAFAFA",
+  },
+  separator: {
+    height: 20,
+  },
+  containerCards: {
+    paddingVertical: 20,
+  },
 });
